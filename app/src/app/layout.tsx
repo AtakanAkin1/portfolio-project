4f0cf813fa89f4f29dee0aa319dea9bceb32c2ac
Atakan Akın <atakan.akin@lbbilisim.com>
2025-07-06 12:25:48 +0300

    Background and background center color modify

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -2,9 +2,6 @@ import type { Metadata } from "next";
 import "@/styles/globals.css";
 import React from "react";
 import "@/styles/font.css";
-import BrowserInfo from "@/components/BrowserInfo";
-import LocationInfo from "@/components/LocationInfo";
-import LocationCountryInfo from "@/components/LocationCountryInfo";
 import MainInfo from "@/components/MainInfo";
 
 export const metadata: Metadata = {
@@ -24,13 +21,14 @@ export default function RootLayout({
           <link
               href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;500;700&family=Titillium+Web:wght@400&display=swap"
               rel="stylesheet"/>
+          <title>Atakan Akın | Full Stack Developer</title>
       </head>
       <body className="container mx-auto p-10 min-h-screen flex flex-col">
-      <div className="background rounded-4xl flex-1 p-5 flex flex-col justify-between">
-         <MainInfo>
-             {children}
-         </MainInfo>
-      </div>
+          <div className="background rounded-4xl flex-1 p-5 flex flex-col justify-between">
+             <MainInfo>
+                 {children}
+             </MainInfo>
+          </div>
       </body>
       </html>
   );
